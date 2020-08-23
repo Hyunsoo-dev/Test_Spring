88188824c7672d26d32c05af861710abeff8b8dc
Hyun Soo <jing07161@gmail.com>
2020-08-23 23:29:43 +0900

    어노테이션 기반 AOP 적용

--- BoardWeb2/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ BoardWeb2/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,11 +1,20 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
 import com.springbook.biz.user.UserVO;
 
+@Service
+@Aspect
 public class AfterReturningAdvice {
+	
+	@AfterReturning(pointcut="PointcutCommon.allPointcut()" , returning="returnObj")
 	public void afterLog(JoinPoint jp, Object returnObj) {
+	
 		String method = jp.getSignature().getName();
 		
 		if(returnObj instanceof UserVO) {
