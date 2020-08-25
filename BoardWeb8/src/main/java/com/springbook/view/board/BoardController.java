8d5df6f4cb7855019ae6fcc0f47580826e72066b
Hyun Soo <jing07161@gmail.com>
2020-08-26 00:29:49 +0900

    프레젠테이션 레이어와 비즈니스 레이어 통합 전까지 커밋

---
+++ BoardWeb8/src/main/java/com/springbook/view/board/BoardController.java
@@ -0,0 +1,73 @@
+package com.springbook.view.board;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.springbook.biz.board.BoardService;
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAO;
+
+@Controller
+@SessionAttributes("board")
+public class BoardController {
+	
+	
+	//검색 조건 목록 설정 
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap(){
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목" , "TITLE");
+		conditionMap.put("내용" , "CONTENT");
+		return conditionMap; //model 객체에 저장됨.
+	}
+	
+	
+	//글 등록
+	@RequestMapping(value="/insertBoard.do")
+	public String insertBoard(BoardVO vo,BoardDAO boardDAO) {
+		System.out.println("글 등록 처리");
+		boardDAO.insertBoard(vo);
+		return "getBoardList.do";
+	}	
+	
+	//글 수정
+	@RequestMapping(value="/updateBoard.do")
+	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+		boardDAO.updateBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	//글 삭제
+	@RequestMapping(value="/deleteBoard.do")
+	public String deleteBoard(BoardVO vo , BoardDAO boardDAO) {
+		System.out.println("글 삭제 처리"); 
+		boardDAO.deleteBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	//글 상세 조회
+	@RequestMapping(value="/getBoard.do")
+	public String getBoard(BoardVO vo, BoardDAO boardDAO,Model model) {
+		System.out.println("글 상세 조회 처리");
+		
+		model.addAttribute("board" , boardDAO.getBoard(vo));
+		return "getBoard.jsp";
+	}
+	//글 목록 검색
+	@RequestMapping(value="/getBoardList.do")
+	public String getBoardList(BoardVO vo, BoardDAO boardDAO,Model model){
+		System.out.println("글 목록 검색 처리");
+		
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+		model.addAttribute("boardList" , boardList);
+		return "getBoardList.jsp";
+	}
+}
